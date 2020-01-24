e7381c35fa33c446319a519021c17aa4a1cc9b87
Guillaume <guillaume.provenzano@archriss.com>
2020-01-24 15:15:06 +0100

    [TASK] updates tests and create snapshots

--- src/Test/App.spec.js
+++ src/Test/App.spec.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { expect } from 'chai'
 import { shallow } from 'enzyme'
-import App from '../App'
+import sinon from 'sinon'
+import App, {SYMBOLS} from '../App'
 import GuessCount from "../App/GuessCount";
 
 describe('<App />', () => {
@@ -14,6 +15,20 @@ describe('<App />', () => {
   it('has 36 cards', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('Card')).to.have.length(36);
+  })
+
+  it('should match its reference snapshot', () => {
+    const mock = sinon
+      .stub(App.prototype, 'generateCards')
+      .returns([...SYMBOLS.repeat(2)]);
+    try {
+      const wrapper = shallow(<App />);
+
+      expect(wrapper).to.matchSnapshot();
+    } finally {
+      mock.restore();
+    }
+
   })
 
 })
--- src/Test/Card.spec.js
+++ src/Test/Card.spec.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import sinon from 'sinon';
 import Card from "../App/Card";
 
 describe('<Card />', () => {
   it('should trigger its `onClick` prop when clicked', () => {
-    const onClick = jest.fn();
+    const onClick = sinon.spy();
     const wrapper = shallow(<Card card="😀" onClick={onClick} feedback="hidden" index={0}/>)
     wrapper.simulate('click');
-    expect(onClick).toHaveBeenCalledWith(0);
+    expect(onClick).to.have.been.calledWith(0);
+  })
+
+  it('should match its reference snapshot', () => {
+    const onClick = sinon.spy();
+    const wrapper = shallow(<Card card="😀" onClick={onClick} feedback="hidden" index={0}/>)
+    expect(wrapper).to.matchSnapshot();
   })
 })
